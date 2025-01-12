578471dbfe0f70c2e5ac7fc30b95f2bce161bd72
Łukasz Kowalewski <l.kowalewski.gaid@gmail.com>
2025-01-12 21:24:15 +0100

    Add viewport functionality

--- screenshot-test-builder.js
+++ screenshot-test-builder.js
@@ -1,8 +1,19 @@
-import { test, expect } from "@playwright/test";
+import { test as t, expect } from '@playwright/test';
 
 class Builder {
   #pageRoute = null;
   #viewName = null;
+  #viewport = ['desktop', 'mobile'];
+  #viewPortResolution = {
+    desktop: {
+      width: 1396,
+      height: 480,
+    },
+    mobile: {
+      width: 600,
+      height: 480,
+    },
+  };
 
   forPage(pageRoute, customViewName) {
     this.#pageRoute = pageRoute;
@@ -10,33 +21,51 @@ class Builder {
     return this;
   }
 
+  forViewports(viewport) {
+    this.#viewport = viewport;
+    return this;
+  }
+
+  async #setViewportFor(viewPort, page) {
+    await page.setViewportSize(this.#viewPortResolution[viewPort]);
+  }
+
   test(variantName) {
+    if (!this.#pageRoute) throw new Error('Page route is not set');
     const testCases = variantName ? [variantName] : [null];
 
-    for (const variant of testCases) {
-      test(this.#getTestDescription(variant), async ({ page }) => {
-        await page.goto(this.#pageRoute);
-
-        await expect(page).toHaveScreenshot(this.#getReferenceFile(variant));
-      
-      });
+    for (const viewPort of this.#viewport) {
+      for (const variant of testCases) {
+        t(
+          this.#getTestDescription(viewPort, variant),
+          async ({ page }) => {
+            await this.#setViewportFor(viewPort, page);
+            await page.goto(this.#pageRoute);
+            await expect(page).toHaveScreenshot(
+              this.#getReferenceFileFor(viewPort, variant)
+            );
+          }
+        );
+      }
     }
 
     return this;
   }
 
-  #getTestDescription(variantName) {
+  #getTestDescription(viewPort, variantName) {
     return [
-      `Compare screenshot for`,
       this.#viewName,
+      ` in @${viewPort} viewport`,
       variantName && `, @${variantName} variant`
-    ].filter(Boolean).join(' ');
+    ]
+      .filter(Boolean)
+      .join('');
   }
 
-  #getReferenceFile(variantName) {
+  #getReferenceFileFor(viewPort, variantName) {
     return [
       this.#viewName,
-      variantName ? `${this.#viewName}-${variantName}` : this.#viewName
+      [this.#viewName, viewPort, variantName].filter(Boolean).join('-')
     ];
   }
 }
