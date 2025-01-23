df5d0527dee7a59aaa29438a114ccf546742acde
Łukasz Kowalewski <l.kowalewski.gaid@gmail.com>
2025-01-23 10:42:23 +0100

    Update screenshots

--- tests/home/home.spec.js
+++ tests/home/home.spec.js
@@ -1,12 +1,17 @@
 //@ts-check
 import ScreenshotTest from '../../screenshotTestBuilder.js';
-
 new ScreenshotTest()
   .forPage('/', 'home')
-  .only()
   .test()
   .setPageInteraction(async (page) => {
-    await page.getByRole('button', { name: 'Book this room' }).click();
+    await page
+      .getByRole('button', {
+        name: 'Book this room',
+      })
+      .nth(1)
+      .click();
+    await page.getByPlaceholder('Firstname').click();
+    await page.getByPlaceholder('Firstname').fill('kk');
   })
   .test('booking')
   .test('anotherVariant')
